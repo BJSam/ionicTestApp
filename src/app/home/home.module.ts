b52ab198073ea8981c6269929d1d37bd40c83008
bjsam-attackio <jbuthuri@attackio.com>
2020-11-01 17:38:18 +0530

    init

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
@@ -16,4 +16,6 @@ import { HomePageRoutingModule } from './home-routing.module';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AlertController, IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  item ={
+    src: '../../../assets/img1.png'
+  };
+  constructor(public alertController: AlertController) {}
+  async Alert(k) {
+   const alert = await this.alertController.create({
+     header: 'clicked',
+     message: k,
+     buttons: ['OK']
+   });
 
+   await alert.present();
+ }
 }
